0763971f5a467a1ec9ae5dfb6816e831824e64e7
learner <roadoflearning@live.com>
2017-03-10 22:12:27 +0800

    添加 Toolbar

--- app/src/main/java/cn/zheteng123/hducreditstatistics/base/BaseActivity.java
+++ app/src/main/java/cn/zheteng123/hducreditstatistics/base/BaseActivity.java
@@ -3,8 +3,11 @@ package cn.zheteng123.hducreditstatistics.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import cn.zheteng123.hducreditstatistics.R;
 import cn.zheteng123.hducreditstatistics.base.interfaces.BaseView;
 
 /**
@@ -14,10 +17,15 @@ import cn.zheteng123.hducreditstatistics.base.interfaces.BaseView;
 
 public abstract class BaseActivity extends AppCompatActivity implements BaseView {
 
+    @BindView(R.id.toolbar)
+    protected Toolbar mToolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResID());
         ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
     }
 }
